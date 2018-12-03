d136cc622101234a5c498f5108d77e4341506f11
mew976 <wangmadie@gmail.com>
2018-12-02 22:08:59 -0500

    Add files via upload

---
+++ addTask.tsx
@@ -0,0 +1,27 @@
+import * as React from 'react';
+
+export default class addTask extends React.Component<any, any> {
+    constructor(props: any){
+        super(props);
+        this.state = { name: this.props.defaultName };
+    }
+
+    public handleOnChange(event: any) : void {
+        this.setState({ name: event.target.value });
+    }
+
+    public render() {
+        return (
+            <div>
+                <div>
+                    <input 
+                        onChange={ e => this.handleOnChange(e) }
+                    />
+                </div>
+                <div>
+                    Hello { this.state.name }!
+                </div>
+            </div>
+        );
+    }
+}
